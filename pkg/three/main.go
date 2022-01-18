c2d933f4b9764ee8f23fd000549b2ae9fc109220
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-01-18 10:32:10 +0100

    Test v0.10.0 move 3

--- pkg/three/main.go
+++ pkg/three/main.go
@@ -2,8 +2,7 @@ package three
 
 import (
 	"github.com/KSerrania/go-test/pkg/one"
-	"github.com/KSerrania/go-test/pkg/two"
 )
 
-// Three is 1 + 2
-const Three = one.One + two.Two
+// Three is 1 + 1 + 1
+const Three = one.One + one.One + one.One
